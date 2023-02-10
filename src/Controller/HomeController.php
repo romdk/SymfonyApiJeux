d041ebed0d9839ce99d2b9175eded71b1e0279c5
romdk <drack.roman67@gmail.com>
2023-02-10 15:37:48 +0100

    symfony api

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use App\HttpClient\BGAHttpClient;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController
+{
+    #[Route('/home', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+    
+
+
+    #[Route('/games', name: 'app_games', methods: ['POST'])]
+    public function displayGames(BGAHttpClient $bga, Request $request) {
+        $search = $request->request->get('searchValue');
+        return new Response($bga->getGames($search));  
+    }
+
+    #[Route('/game', name: 'app_game', methods: ['POST'])]
+    public function displayGame(BGAHttpClient $bga, Request $request) {
+        $search = $request->request->get('gameId');
+        return new Response($bga->getGame($search));        
+    }
+}
